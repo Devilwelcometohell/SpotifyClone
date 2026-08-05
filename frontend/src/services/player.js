363d3b042e841e7ae97059797c9c50eec8557fff
Devang Shukla <devangshukla218@gmail.com>
2026-08-05 15:32:39 +0530

    Update player.js

--- frontend/src/services/player.js
+++ frontend/src/services/player.js
@@ -1,10 +1,7 @@
-import axios from "axios";
-
-const API = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+import api from "./api";
 
 export async function playSong(song) {
-
-    const response = await axios.get(`${API}/player/play`, {
+    const response = await api.get("/player/play", {
         params: {
             song: song.title,
             artist: song.artist,
